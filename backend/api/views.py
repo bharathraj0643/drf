8612365a04ab5697d878ac5948d7b91913572b66
Bharath Raj S <bharathraj0643@gmail.com>
2025-05-23 15:11:31 +0530

    session auth and permissions

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 
-from rest_framework import viewsets
+from rest_framework import viewsets , permissions , authentication
 from products.models import Product
 from . serializers import ProductSerializer
 
 class ProductViewset(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    authentication_classes = [authentication.SessionAuthentication]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
