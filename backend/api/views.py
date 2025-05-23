01b4a27af34af0d9badbe3131d26e859e76cb347
Bharath Raj S <bharathraj0643@gmail.com>
2025-05-23 16:40:27 +0530

    custom permissions

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,9 +3,10 @@ from django.shortcuts import render
 from rest_framework import viewsets , permissions , authentication
 from products.models import Product
 from . serializers import ProductSerializer
+from products.permissions import IsStaffEditorPermission
 
 class ProductViewset(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     authentication_classes = [authentication.SessionAuthentication]
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [ permissions.IsAdminUser , IsStaffEditorPermission]
